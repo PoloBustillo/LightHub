cdc289c3dfcbdf0c9017c7f0f44333d1b6c1e6a1
PoloBustillo <leopoldobeuiluz1@hotmail.com>
2023-10-09 17:33:08 -0600

    display eerrrrr

--- light-hub/app/login/page.jsx
+++ light-hub/app/login/page.jsx
@@ -13,6 +13,7 @@ import { useRouter } from "next/navigation";
 import { getProviders, signIn, useSession } from "next-auth/react";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { loginSchema } from "@/utils/validations";
+import { API_URL } from "@/utils/constants";
 
 export default function RegisterPage() {
   const {
@@ -46,9 +47,9 @@ export default function RegisterPage() {
       password: data.password,
       callbackUrl: "/",
     });
-
+    console.log(API_URL);
     Object.keys(error).forEach((e) =>
-      console.log(`key=${e}  value=${yourObj[e]}`)
+      console.log(`key=${e}  value=${error[e]}`)
     );
     console.log(JSON.stringify(error));
 

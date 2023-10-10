4be4df857f2c847f31070e63f1b88e4e8c6e2510
PoloBustillo <leopoldobeuiluz1@hotmail.com>
2023-10-09 20:36:00 -0600

    update logger to not user prettier

--- light-hub/app/logger.js
+++ light-hub/app/logger.js
@@ -1,19 +1,11 @@
 import pino from "pino";
-import pretty from "pino-pretty";
 
-const stream = pretty({
-  colorize: true,
-  translateTime: "SYS:standard",
-});
-export default pino(
-  {
-    level: process.env.PINO_LOG_LEVEL || "info",
-    formatters: {
-      level: (label) => {
-        return { level: label.toUpperCase() };
-      },
+export default pino({
+  level: process.env.PINO_LOG_LEVEL || "info",
+  formatters: {
+    level: (label) => {
+      return { level: label.toUpperCase() };
     },
-    timestamp: () => `,"timestamp":"${new Date(Date.now()).toISOString()}"`,
   },
-  stream
-);
+  timestamp: () => `,"timestamp":"${new Date(Date.now()).toISOString()}"`,
+});

e8a38a4689e37b6d1c1758dad5c07b50c1585594
PoloBustillo <leopoldobeuiluz1@hotmail.com>
2023-10-09 20:24:48 -0600

    remove old dbs

--- light-hub/app/api/hello/route.js
+++ light-hub/app/api/hello/route.js
@@ -3,7 +3,7 @@ import { connectSQLDB } from "@/lib/db_sql";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  await connectDB();
-  await connectSQLDB();
+  // await connectDB();
+  // await connectSQLDB();
   return NextResponse.json({ saludo: "Hola Polo" });
 }

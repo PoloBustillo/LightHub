24efe2cd537ef651b785a04070f0ccd8f5c9facf
PoloBustillo <leopoldobeuiluz1@hotmail.com>
2023-10-12 23:17:13 -0600

    disable sentry

--- light-hub/sentry.client.config.js
+++ light-hub/sentry.client.config.js
@@ -4,27 +4,29 @@
 
 import * as Sentry from "@sentry/nextjs";
 
-Sentry.init({
-  dsn: "https://d993d876d7915920dc1308e0ef2963f8@o4504188465709056.ingest.sentry.io/4505924918640640",
+if (process.env.NODE_ENV !== "production") {
+  Sentry.init({
+    dsn: "https://d993d876d7915920dc1308e0ef2963f8@o4504188465709056.ingest.sentry.io/4505924918640640",
 
-  // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 1,
+    // Adjust this value in production, or use tracesSampler for greater control
+    tracesSampleRate: 1,
 
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: true,
+    // Setting this option to true will print useful information to the console while you're setting up Sentry.
+    debug: true,
 
-  replaysOnErrorSampleRate: 1.0,
+    replaysOnErrorSampleRate: 1.0,
 
-  // This sets the sample rate to be 10%. You may want this to be 100% while
-  // in development and sample at a lower rate in production
-  replaysSessionSampleRate: 0.1,
+    // This sets the sample rate to be 10%. You may want this to be 100% while
+    // in development and sample at a lower rate in production
+    replaysSessionSampleRate: 0.1,
 
-  // You can remove this option if you're not planning to use the Sentry Session Replay feature:
-  integrations: [
-    // new Sentry.Replay({
-    //   // Additional Replay configuration goes in here, for example:
-    //   maskAllText: false,
-    //   blockAllMedia: false,
-    // }),
-  ],
-});
+    // You can remove this option if you're not planning to use the Sentry Session Replay feature:
+    integrations: [
+      // new Sentry.Replay({
+      //   // Additional Replay configuration goes in here, for example:
+      //   maskAllText: false,
+      //   blockAllMedia: false,
+      // }),
+    ],
+  });
+}
